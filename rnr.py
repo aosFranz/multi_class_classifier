d47a342f682d46288a5cb832605e438550cb6765
AndrewF1191 <franz_andreas@gmx.de>
2017-12-29 16:22:25 +0100

    running model

--- rnr.py
+++ rnr.py
@@ -8,7 +8,21 @@ from keras.preprocessing.text import Tokenizer
 import numpy as np
 import math
 
-raw_data = pd.read_csv('/home/andreas/Data/Kaggle/Toxic_Comment/train.csv')
+raw_data = pd.read_csv('/home/andreas/Data/Kaggle/Toxic_Comment/train.csv')[:1020]
+
+list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+raw_labels = raw_data[list_classes].values
+
+labels = []
+for raw_label in raw_labels:
+    if all(i == 0 for i in raw_label):
+        full_label = np.append(raw_label,1)
+        labels.append(full_label)
+    else:
+        full_label = np.append(raw_label,0)
+        labels.append(full_label)
+labels = np.array(labels)
+
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(raw_data['comment_text'])
 tokenized = tokenizer.texts_to_sequences(raw_data['comment_text'])
@@ -18,18 +32,26 @@ def create_batches_of_size(list,size):
     batches = []
     i = 0
     while i < math.ceil(len(list) / size):
-        batch_i = list[i * size:(i + 1) * size]
+        batch_i = np.array(list[i * size:(i + 1) * size])
         batches.append(batch_i)
         i += 1
     return batches
 
-batches = create_batches_of_size(tokenized,1000)
-tfidf = [tokenizer.sequences_to_matrix(batch,mode='tfidf') for batch in batches]
+batches_x = create_batches_of_size(tokenized,1000)
+batches_y = create_batches_of_size(labels,1000)
+tfidf_batches = [tokenizer.sequences_to_matrix(batch,mode='tfidf') for batch in batches_x]
 
-print(len(tokenizer.__dict__))
-print(len(tfidf[0][0]))
+tfidfed = np.array([x for list in tfidf_batches for x in list])
 
 model = Sequential()
-model.add(Dense(512,input_dim=len(tokenizer.__dict__)))
+model.add(Dense(512,input_dim=tfidf_batches[0].shape[1]))
+model.add(Activation('relu'))
+model.add(Dense(7))
+model.add(Activation('softmax'))
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
+
+model.fit(x=tfidfed,y=labels,batch_size=1000,epochs=10000)
 
